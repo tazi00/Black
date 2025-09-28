a70759a9c40b3ea78bc357a2d6245f84fede1ab0
Tushar Dutta <tz0540245@gmail.com>
2025-09-28 10:11:51 +0530

    layout file css fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
-import '@/app/global.css';
+import './global.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
